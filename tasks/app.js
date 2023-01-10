46dde95df7440de749ee7a224bf9187f2cc54742
Gabriel <gabriel.rockson@amalitech.com>
2023-01-10 14:57:06 +0000

    feat: add api endpoint to get a task by id

--- tasks/app.js
+++ tasks/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const tasks = require("./data");
 
 const app = express();
 const port = 5000;
+
+// create applicaion/json parser
+const jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Welcome to node" });
@@ -10,6 +17,17 @@ app.get("/", (req, res) => {
 
 app.get("/tasks", (req, res) => {
   res.status(200).send(tasks);
+});
+
+app.get("/tasks/:taskId", (req, res) => {
+  const taskId = req.params.taskId;
+  task = tasks.find((task) => task.id === taskId);
+
+  if (!task) {
+    res.status(404).send({ detail: `Task with id ${task} is not found` });
+  }
+
+  res.status(200).send(task);
 });
 
 app.listen(port, () => {
