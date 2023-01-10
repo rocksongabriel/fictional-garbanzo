86e2a77e66650d295eadbe06f218b7af67906825
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-01-10 14:24:00 +0000

    feat: add static tasks data

---
+++ tasks/data.js
@@ -0,0 +1,44 @@
+const tasks = [
+  {
+    id: "1a2b3c4d-5e6f-7g8h-9i0j-1k2l3m4n5o6p",
+    owner_id: "q1r2s3t4-u5v6w7x8-y9z0a1b2-c3d4e5f6",
+    task: "Task 1",
+    description: "This is the first task",
+    created: "2022-01-01 12:00:00",
+    deadline: "2022-01-01 14:00:00",
+  },
+  {
+    id: "7g8h9i0j-1k2l3m4n-5o6p7q8r-9s0t1u2v",
+    owner_id: "w1x2y3z4-a1b2c3d4-e5f6g7h8-i9j0k1l2",
+    task: "Task 2",
+    description: "This is the second task",
+    created: "2022-01-02 12:00:00",
+    deadline: "2022-01-02 14:00:00",
+  },
+  {
+    id: "3m4n5o6p-7q8r9s0t-1u2v3w4x-5y6z7a8b",
+    owner_id: "c1d2e3f4-g5h6i7j8-k9l0m1n2-o3p4q5r6",
+    task: "Task 3",
+    description: "This is the third task",
+    created: "2022-01-03 12:00:00",
+    deadline: "2022-01-03 14:00:00",
+  },
+  {
+    id: "5o6p7q8r-9s0t1u2v-3w4x5y6z-7a8b9c0d",
+    owner_id: "p1q2r3s4-t5u6v7w8-x9y0z1a2-b3c4d5e6",
+    task: "Task 4",
+    description: "This is the fourth task",
+    created: "2022-01-04 12:00:00",
+    deadline: "2022-01-04 14:00:00",
+  },
+  {
+    id: "7q8r9s0t-1u2v3w4x-5y6z7a8b-9c0d1e2f",
+    owner_id: "r1s2t3u4-v5w6x7y8-z9a0b1c2-d3e4f5g6",
+    task: "Task 5",
+    description: "This is the fifth task",
+    created: "2022-01-05 12:00:00",
+    deadline: "2022-01-05 14:00:00",
+  },
+];
+
+module.exports = tasks;
